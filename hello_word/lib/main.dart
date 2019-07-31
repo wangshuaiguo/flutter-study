02f73a37bf827a846662724a1f4a85cb0ac46a50
727881945@qq.com <314>
2019-07-31 15:34:24 +0800

    画板

--- hello_word/lib/main.dart
+++ hello_word/lib/main.dart
@@ -1,63 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'My App',
-    home: MyScaffold(),
-  ));
+class SignaturePainter extends CustomPainter {
+  SignaturePainter(this.points);
+
+  final List<Offset> points;
+
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null)
+        canvas.drawLine(points[i], points[i + 1], paint);
+    }
+  }
+
+  bool shouldRepaint(SignaturePainter other) => other.points != points;
 }
 
-class MyAppBar extends StatelessWidget {
-  // Widget子类中的字段往往都会定义为"final"
-  final Widget title;
+class Signature extends StatefulWidget {
+  SignatureState createState() => new SignatureState();
+}
 
-  MyAppBar(this.title);
+class SignatureState extends State<Signature> {
+  List<Offset> _points = <Offset>[];
 
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 84.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'navigation menu',
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          )
-        ],
-      ),
+    return new Stack(
+      children: [
+        GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            RenderBox referenceBox = context.findRenderObject();
+            Offset localPosition =
+                referenceBox.globalToLocal(details.globalPosition);
+
+            setState(() {
+              _points = new List.from(_points)..add(localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+        ),
+        CustomPaint(painter: new SignaturePainter(_points))
+      ],
     );
   }
 }
 
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Material 是UI呈现的“一张纸”
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(Text(
-            'Example title',
-            style: Theme.of(context).primaryTextTheme.title,
-          )),
-          Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
-          )
-        ],
-      ),
-    );
-  }
+class DemoApp extends StatelessWidget {
+  Widget build(BuildContext context) => new Scaffold(body: new Signature());
 }
+
+void main() => runApp(new MaterialApp(home: new DemoApp()));
